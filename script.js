67c1a15f21c5420aff81e038c172337ab14a84ca
SaraSomy <121504024+SaraSomy@users.noreply.github.com>
2024-09-11 23:01:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let اسمالمستخدمRef = document.getElementById("اسم المستخدم");
+let كلمةالمرورRef = document.getElementById("كلمة المرور");
+let eyeL = document.querySelector(".eyeball-l");
+let eyeR = document.querySelector(".eyeball-r");
+let handL = document.querySelector(".hand-l");
+let handR = document.querySelector(".hand-r");
+
+let normalEyeStyle = () => {
+  eyeL.style.cssText = `
+    left:0.6em;
+    top: 0.6em;
+  `;
+  eyeR.style.cssText = `
+  right:0.6em;
+  top:0.6em;
+  `;
+};
+
+let normalHandStyle = () => {
+  handL.style.cssText = `
+        height: 2.81em;
+        top:8.4em;
+        left:7.5em;
+        transform: rotate(0deg);
+    `;
+  handR.style.cssText = `
+        height: 2.81em;
+        top: 8.4em;
+        right: 7.5em;
+        transform: rotate(0deg)
+    `;
+};
+//When clicked on اسم المستخدم input
+اسمالمستخدمRef.addEventListener("focus", () => {
+  eyeL.style.cssText = `
+    left: 0.75em;
+    top: 1.12em;  
+  `;
+  eyeR.style.cssText = `
+    right: 0.75em;
+    top: 1.12em;
+  `;
+  normalHandStyle();
+});
+//When clicked on كلمةالمرور input
+كلمةالمرورRef.addEventListener("focus", () => {
+  handL.style.cssText = `
+        height: 6.56em;
+        top: 3.87em;
+        left: 11.75em;
+        transform: rotate(-155deg);    
+    `;
+  handR.style.cssText = `
+    height: 6.56em;
+    top: 3.87em;
+    right: 11.75em;
+    transform: rotate(155deg);
+  `;
+  normalEyeStyle();
+});
+//When clicked outside username and كلمة المرور input
+document.addEventListener("click", (e) => {
+  let clickedElem = e.target;
+  if (clickedElem != usernameRef && clickedElem != passwordRef) {
+    normalEyeStyle();
+    normalHandStyle();
+  }
+});
---
+++ sign up.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"> 
+    <title>طفولة</title>
+    <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap" rel="stylesheet">"
+    <link rel="icon" href="https://e7.pngegg.com/pngimages/502/39/png-clipart-colored-pencil-pencil-pencil-pen-thumbnail.png">
+    </head>
+    <style type="style.css">
+   
+
+ 
